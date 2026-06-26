c35597dd7794a02345828d3ee94631559ac984a6
thimaichidoan <40016084@parisnanterre.fr>
2026-06-26 14:43:40 +0200

    Initialisation du projet Spring Boot

---
+++ currency-api/src/test/java/com/miage/currencyapi/CurrencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.currencyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
